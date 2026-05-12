5c7c2186bc52ca0c6a97fe3fe8fa7ec0761e7a44
Iman-nami <iman.obenali@student.ehb.be>
2026-05-12 21:49:14 +0200

    Connect DOM elements to Javascript

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './style.css'
 const API_KEY = 'cbc56582dd79a5ca1e7d693e2a193233';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500';
+
+
+const dramaGrid = document.querySelector('#dramaGrid');
+const totalCount = document.querySelector('#totalCount');
+const averageRating = document.querySelector('#averageRating');
+const favoritesCount = document.querySelector('#favoritesCount');
